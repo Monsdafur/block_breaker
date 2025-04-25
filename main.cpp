7cbe53918b77ac2b76dcf77c60546cc55b25e4d4
Hoa <dqhoa06@gmail.com>
2025-04-25 17:33:28 +0700

    switched to C++ 23

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/RenderTexture.hpp>
-#include <SFML/Graphics/Shader.hpp>
-#include <SFML/Graphics/Sprite.hpp>
-#include <SFML/Graphics/Texture.hpp>
-#include <SFML/System/Angle.hpp>
-#include <SFML/System/Vector2.hpp>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode({1600, 800}), "bock_breaker");
